436cf365b13b56c001ea2a3673639785f786fc98
Jerrico Gamis <jecklgamis@gmail.com>
2023-04-20 06:23:54 +1000

    Upgrade to cucumber-7.11.2, refactor deprecated property

--- src/main/java/steps/ExampleInjectorSource.java
+++ src/main/java/steps/ExampleInjectorSource.java
@@ -1,10 +1,11 @@
-package util;
+package steps;
 
 import com.google.inject.Guice;
 import com.google.inject.Injector;
 import com.google.inject.Stage;
 import io.cucumber.guice.CucumberModules;
 import io.cucumber.guice.InjectorSource;
+import util.ExampleTestModule;
 
 public class ExampleInjectorSource implements InjectorSource {
     @Override
--- src/main/java/util/ExampleTestModule.java
+++ src/main/java/util/ExampleTestModule.java
@@ -3,7 +3,7 @@ package util;
 import com.google.inject.AbstractModule;
 import io.cucumber.guice.ScenarioScoped;
 
-class ExampleTestModule extends AbstractModule {
+public class ExampleTestModule extends AbstractModule {
     @Override
     protected void configure() {
         bind(TestContext.class).in(ScenarioScoped.class);
